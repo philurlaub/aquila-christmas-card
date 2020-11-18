022c063949df11e8f2abea1b7cc226bc8866acd1
Philipp Paul <mail@philipp-paul.de>
2020-11-18 12:04:09 +0100

    Progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NxButtonModule } from '@aposin/ng-aquila/button';
+import { NxCardModule } from '@aposin/ng-aquila/card';
+import { NxFooterModule } from '@aposin/ng-aquila/footer';
+import { NxGridModule } from '@aposin/ng-aquila/grid';
+import { NxHeaderModule } from '@aposin/ng-aquila/header';
+import { NxLinkModule } from '@aposin/ng-aquila/link';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NxIconModule} from '@aposin/ng-aquila/icon';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NxHeaderModule,
+    NxFooterModule,
+    NxButtonModule,
+    NxLinkModule,
+    NxGridModule,
+    NxCardModule,
+    NxIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
